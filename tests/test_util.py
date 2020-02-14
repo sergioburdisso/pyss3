a0131f019dbc8b6d00a67f60a35d5e5c24da2241
Sergio Burdisso <sergio.burdisso@gmail.com>
2020-02-14 18:35:18 -0300

    Add tests for the pyss3.server module

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,25 +7,29 @@ import pytest
 DATASET_FOLDER = "dataset_mr"
 
 dataset_path = path.join(path.abspath(path.dirname(__file__)), DATASET_FOLDER)
-x_train, y_train = Dataset.load_from_files(dataset_path, folder_label=True)
 
-rd = RecursiveDefaultDict()
-rd["a"]["new"]["element"] = "assigned"
 
-Print.set_verbosity(VERBOSITY.VERBOSE)
-Print.verbosity_region_begin(VERBOSITY.VERBOSE)
+def test_util():
+    """Test utility module."""
+    x_train, y_train = Dataset.load_from_files(dataset_path, folder_label=True)
 
-print(Print.style.header("this is a header!"))
+    rd = RecursiveDefaultDict()
+    rd["a"]["new"]["element"] = "assigned"
 
-Print.warn("This is a warning!")
-Print.info("This is an informative message!")
-Print.show("This is a message!")
+    Print.set_verbosity(VERBOSITY.VERBOSE)
+    Print.verbosity_region_begin(VERBOSITY.VERBOSE)
 
-with pytest.raises(Exception):
-    Print.warn("This is a warning!", raises=Exception)
+    print(Print.style.header("this is a header!"))
 
-Print.set_decorator_info(">", "<")
-Print.set_decorator_warn("|", "|")
-Print.set_decorator_error("*", "*")
+    Print.warn("This is a warning!")
+    Print.info("This is an informative message!")
+    Print.show("This is a message!")
 
-Print.verbosity_region_end()
+    with pytest.raises(Exception):
+        Print.warn("This is a warning!", raises=Exception)
+
+    Print.set_decorator_info(">", "<")
+    Print.set_decorator_warn("|", "|")
+    Print.set_decorator_error("*", "*")
+
+    Print.verbosity_region_end()
