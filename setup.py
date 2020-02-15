40ad86fe3a5a59c19eadbbff96a003b927375114
Sergio Burdisso <sergio.burdisso@gmail.com>
2020-02-15 20:10:59 -0300

    Revert "Try a best solution for 0% coverage issue"
    This reverts commit 47677dad33daeb93b39867e29c71ee953208efca.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
                             'matplotlib'],
           tests_require=['pytest',
                          'pytest-mock'
-                         'pytest-cov>=2.8.1'
+                         'pytest-cov>=2.5'
                          'codecov',
                          'flake8',
                          'six',
