8a149a9a2803053dc7c04c5a2fc1d85de407e03c
Sergio Burdisso <sergio.burdisso@gmail.com>
2020-02-27 14:52:29 -0300

    Release version 0.5.4

--- pyss3/__init__.py
+++ pyss3/__init__.py
@@ -20,7 +20,7 @@ from .util import Print, VERBOSITY, Preproc as Pp
 from functools import reduce
 from six.moves import xrange
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 ENCODING = "utf-8"
 
