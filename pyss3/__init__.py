cae979e141da097dd727fc347d34b5832114234a
Sergio Burdisso <sergio.burdisso@gmail.com>
2020-06-19 19:26:57 -0300

    Release version 0.6.2
    - Fix ZeroDivisionError bug (#10)

--- pyss3/__init__.py
+++ pyss3/__init__.py
@@ -21,7 +21,7 @@ from .util import is_a_collection, Print, VERBOSITY, Preproc as Pp
 from functools import reduce
 from six.moves import xrange
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 ENCODING = "utf-8"
 
