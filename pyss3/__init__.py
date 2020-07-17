544ecb609f5059a4a9526da46aa7dc0be20ac63d
Sergio Burdisso <sergio.burdisso@gmail.com>
2020-07-17 17:44:25 -0300

    Release version 0.6.3
    Patches issue #11

--- pyss3/__init__.py
+++ pyss3/__init__.py
@@ -21,7 +21,7 @@ from .util import is_a_collection, Print, VERBOSITY, Preproc as Pp
 from functools import reduce
 from six.moves import xrange
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 ENCODING = "utf-8"
 
